5b8de1f96cbaa502adfa7df25ab7f6ec2e65af9e
Maslor <gabriel.freire@tecnico.ulisboa.pt>
2016-04-03 03:41:22 +0100

    new version of the site

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
   $("#menu-items").hide();
   $("#m-sub-menu").hide();
+  $("#history").hide();
+  $("#pedido").hide();
+  $("#encomendar").hide();
+  
   $("#m").click(function() {
     $("#m-sub-menu").toggle();
   });
   
   $("#menu").click(function() {
     $("#menu-items").toggle();
+    $("#pedido").toggle();
+    $("#encomendar").toggle();
+  });
+  
+  $("#history-btn").click(function() {
+    $("#history").toggle();
   });
   
 });
