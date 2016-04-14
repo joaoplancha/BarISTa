e73d5c8418566baaec5c6893a6962f32361dbf7a
Maslor <gabriel.freire@tecnico.ulisboa.pt>
2016-04-14 10:38:15 +0100

    single select implemented

--- js/index.js
+++ js/index.js
@@ -286,7 +286,14 @@ $(document).ready(function() {
 
 
   $('#ementa tbody').on('click', 'tr', function() {
-    $(this).toggleClass('selected');
+     var table = $('#ementa').DataTable();
+    if ( $(this).hasClass('selected') ) {
+            $(this).removeClass('selected');
+        }
+        else {
+            table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
     addItemToPedido($(this).find('td:eq(0)').text(), $(this).find('td:eq(1)').text());
   });
 });
