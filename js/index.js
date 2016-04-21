e94e1e32ead16a55bd3d7d089972b5f57dc2cba2
Maslor <gabriel.freire@tecnico.ulisboa.pt>
2016-04-21 11:30:46 +0100

    closes #21

--- js/index.js
+++ js/index.js
@@ -102,7 +102,7 @@
 
   function continueToAdd() {
 
-    var row = table.insertRow(table.rows.length - 1);
+    var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
