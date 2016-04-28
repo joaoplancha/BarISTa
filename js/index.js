fd0e3952f875bb7e09949a8f3c90d8dfea1e12b3
Maslor <gabriel.freire@tecnico.ulisboa.pt>
2016-04-28 13:40:05 +0100

    added range date picker. Functionality not implemented yet

--- js/index.js
+++ js/index.js
@@ -313,8 +313,21 @@
       }
     });
     
-    $( "#datepicker" ).datepicker({
-       dateFormat: "dd/mm/yy"
+    $( "#from" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 3,
+      onClose: function( selectedDate ) {
+        $( "#to" ).datepicker( "option", "minDate", selectedDate );
+      }
+    });
+    $( "#to" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 3,
+      onClose: function( selectedDate ) {
+        $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+      }
     });
 
     function emPreparacao() {
